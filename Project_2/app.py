63a276fe5df36e4f47b5ef882dc920d21f1f15f7
John Woodruff <104457150+Jswoodruff@users.noreply.github.com>
2022-09-14 10:37:08 -0700

    Update app.py

--- Project_2/app.py
+++ Project_2/app.py
@@ -6,9 +6,9 @@ from src import pht
 
 st.title('Seattle PHT Metrics')
 r,s,t = pht.run_app()
-st.write(f'PHT fails {r}')
-st.write(f'PHT Pass {s}')
-st.write(f'PHT Pass w/ Warnings {t}')
+st.write(f'PHT fails: {r}')
+st.write(f'PHT Pass: {s}')
+st.write(f'PHT Pass with Warnings: {t}')
 
 # try:
 #     r,s,t = pht.run_app()
